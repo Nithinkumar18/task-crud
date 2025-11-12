7c4ce8b0a4e160467b969189d999649ce71961ff
Nithin <vooturinithinkumar@gmail.com>
2025-11-12 10:52:00 +0530

    import auth,task & team module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './tasks/task.module';
+import { TeamModule } from './team/team.module';
+import {Task} from './tasks/task.entity';
+import {Team} from './team/team.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URI,
+     synchronize: true,
+     entities: [Task,Team]
+
+    }),
+    AuthModule,TaskModule,TeamModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
